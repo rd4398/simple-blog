dfd159deeea70fa82410ae2d407ea18dfa56228c
Rohan Devasthale <rdevasthale98@gmail.com>
2023-10-29 21:37:03 -0400

    Added Nav and Home components and applied basic css styles for Nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1> Rohan's Blog </h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/createBlog">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
