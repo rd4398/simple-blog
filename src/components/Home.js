45b02f7ad261046abac40e5dd344a3d228953d33
Rohan Devasthale <rdevasthale98@gmail.com>
2023-10-30 15:26:11 -0400

    Adding list of blog posts and css for the list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,21 @@
+import { useState} from 'react';
+
 const Home = () => {
+
+    const [blogPosts, setBlogPosts] = useState([
+        { title: 'The first blog', body: 'lorem ipsum...', author: 'mario', id: 1},
+        { title: 'The Evening Party', body: 'lorem ipsum...', author: 'rohan', id: 2},
+        { title: 'Trending React', body: 'lorem ipsum...', author: 'sam', id: 3},
+    ]);
+
     return (
         <div className="home">
-            <h2>Home Page</h2>
+            {blogPosts.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
     );
 }
