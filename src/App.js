2cc8e2abc8d74fa0aff066ca127134c8122471d5
Rohan Devasthale <rdevasthale98@gmail.com>
2023-11-01 13:34:10 -0400

    Adding react router and route for home component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   // Reat cannot output objects and boolean
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
