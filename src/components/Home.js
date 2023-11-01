06a3a8c030e80af9c909930c1ddefd71c7c8f2bc
Rohan Devasthale <rdevasthale98@gmail.com>
2023-11-01 13:21:25 -0400

    Creating a custom hook to fetch data for reuability

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,9 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from '../useFetch';
 
 const Home = () => {
-
-    const [blogs, setBlogPosts] = useState(null);
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
-
-    // Dependency array used to control when useEffect runs
-    useEffect(()=>{
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-        .then(response => {
-            if(!response.ok) {
-                throw Error('Could not fetch the data for the requested resource')
-            }
-            return response.json();
-        })
-        .then(data => {
-            setBlogPosts(data);
-            setIsPending(false);
-            setError(null)
-        })
-        .catch(err => {
-            setIsPending(false);
-            setError(err.message)
-        })
-        }, 1000)
-    }, [])   
-
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
+    
     return (
         <div className="home">
             { error && <div>{ error }</div>}
